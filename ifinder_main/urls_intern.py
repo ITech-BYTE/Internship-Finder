32b1f42454cfd2d650d625d7b847cd4570dbfbbc
David Steiner <david_j_steiner@yahoo.co.nz>
2014-02-22 23:14:11 +0000

    fixed company registration page

--- ifinder_main/urls_intern.py
+++ ifinder_main/urls_intern.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from ifinder_main import views
 
 urlpatterns = patterns('',
-        url(r'^register/$', views.register, name='register'),
+        url(r'^register/$', views.register_intern, name='register'),
         url(r'^offer/(?P<internship_id>\w+)/$', views.internship_details, name="internship_details")
 )
--- ifinder_main/views.py
+++ ifinder_main/views.py
@@ -22,19 +22,37 @@ def user_logout(request):
 
     return HttpResponseRedirect('/')
 
+
 # INTERN REGISTRATION
-def register(request):
+def register_intern(request):
+    return register(request, 0)
+
+# COMPANY REGISTRATION
+def register_company(request):
+    return register(request, 1)
+
+# GENERAL REGISTRATION METHOD (interns: user_type=0, companies: user_type=1)
+def register(request, user_type):
     context = RequestContext(request)
 
     # A boolean value for telling the template whether the registration was successful.
     registered = False
+
+    if user_type == 0:
+            action = "/intern/register/"
+    elif user_type == 1:
+            action = "/company/register/"
 
     # If it's a HTTP POST, we're interested in processing form data.
     if request.method == 'POST':
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         user_form = UserForm(data=request.POST)
-        profile_form = InternForm(data=request.POST)
+
+        if user_type == 0:
+            profile_form = InternForm(data=request.POST)
+        elif user_type == 1:
+            profile_form = CompanyForm(data=request.POST)
 
         # If the two forms are valid...
         if user_form.is_valid() and profile_form.is_valid():
@@ -68,69 +86,15 @@ def register(request):
     # These forms will be blank, ready for user input.
     else:
         user_form = UserForm()
-        profile_form = InternForm()
+        if user_type == 0:
+            profile_form = InternForm()
+        elif user_type == 1:
+            profile_form = CompanyForm()
 
     # Render the template depending on the context.
     return render_to_response(
         'register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
-            context)
-
-
-# COMPANY REGISTRATION
-def register_company(request):
-    # Like before, get the request's context.
-    context = RequestContext(request)
-
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
-
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
-        company_form = CompanyForm(data=request.POST)
-
-        # If the two forms are valid...
-        if user_form.is_valid() and company_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
-
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = company_form.save(commit=False)
-            profile.user = user
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
-            # Update our variable to tell the template registration was successful.
-            registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors, company_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserForm()
-        company_form = CompanyForm()
-
-    # Render the template depending on the context.
-    return render_to_response(
-        'register.html',
-            {'user_form': user_form, 'profile_form': company_form, 'registered': registered},
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered, 'action' : action},
             context)
 
 
